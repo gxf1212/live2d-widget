51f2a0fb7fcf66606fc05972aa9ce8237a3a059a
vege-chicken <48995577+gxf1212@users.noreply.github.com>
2023-02-18 18:45:36 +0800

    try debug

--- waifu-tips.js
+++ waifu-tips.js
@@ -64,7 +64,8 @@ function loadWidget(config) {
 						"出现这幅思维图景时，我正在想你。",
 						"一样东西，要是离我们心中的概念差得太远，可不是好兆头。",
 						"在这寒冷广漠的宇宙中，同为碳基生命本身就是一种缘分，一种可能要几十亿年才能修得的缘分。",
-						"很好地与这种环境融为一体的人，同时也对它产生了依赖感"];
+						"很好地与这种环境融为一体的人，同时也对它产生了依赖感", 
+						"时空的薄膜在引力波中泛起一片涟漪，像风吹皱了暗夜中的湖面。"];
 	window.addEventListener("mousemove", () => userAction = true);
 	window.addEventListener("keydown", () => userAction = true);
 	setInterval(() => {
